9fbc97d68d47da2208764d27e245e310a26344e4
Roberto Arista <arista.rob@gmail.com>
2024-02-29 10:55:21 +0100

    unpack extension and add github action

--- build/Tracer.roboFontExt/lib/menu_openFontWindow.py
+++
@@ -1,9 +0,0 @@
-from vanilla import dialogs
-from tracer.window import TracerWindowController
-
-if __name__ == "__main__":
-    font = CurrentFont()
-    if font is None:
-        dialogs.message("A font must be open to run Tracer.")
-    else:
-        TracerWindowController(font=font)
--- build/Tracer.roboFontExt/lib/tracer/__init__.py
+++
@@ -1,7 +0,0 @@
-from .trace import traceGlyphImage
-from .simplify import (
-    simplifyGlyphContours,
-    countGlyphPoints,
-    SimplifyContoursPen,
-    CountPen
-)
